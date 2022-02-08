f2da2f65c98251e0c61c5c501f7f85f9ed47badb
Waldemar Ewert <waldi.ewert@googlemail.com>
2022-02-08 22:16:34 +0100

    create spring boot project

---
+++ src/main/java/de/ewertw/costtrackingbe/CostTrackingBeApplication.java
@@ -0,0 +1,13 @@
+package de.ewertw.costtrackingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostTrackingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostTrackingBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/ewertw/costtrackingbe/CostTrackingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ewertw.costtrackingbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostTrackingBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
