f79420189c93fe5b6c762ceed8ebd58926af1a4b
Waldemar Ewert <waldi.ewert@googlemail.com>
2022-02-18 22:03:27 +0100

    transaktionMapper bugfix

--- src/main/java/de/ewertw/costtrackingbe/mapper/costtracking/TransaktionMapper.java
+++ src/main/java/de/ewertw/costtrackingbe/mapper/costtracking/TransaktionMapper.java
@@ -4,10 +4,22 @@ import de.ewertw.costtrackingbe.models.costtracking.Transaktion;
 import de.ewertw.costtrackingbe.viewmodels.costtracking.TransaktionDto;
 import de.ewertw.costtrackingbe.viewmodels.costtracking.TransaktionInsertDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface TransaktionMapper {
+    @Mappings({
+            @Mapping(target="kostenart.kostenartname", source = "kostenart"),
+            @Mapping(target="kostenbereich.kostenbereichname", source = "kostenbereich"),
+            @Mapping(target="kostentraeger.kostentraegername", source = "kostentraeger")
+    })
     Transaktion transaktionInsertDtoToTransaktion (TransaktionInsertDto transaktionInsertDto);
+    @Mappings({
+            @Mapping(target="kostenart", source = "kostenart.kostenartname"),
+            @Mapping(target="kostenbereich", source = "kostenbereich.kostenbereichname"),
+            @Mapping(target="kostentraeger", source = "kostentraeger.kostentraegername")
+    })
     TransaktionDto transaktionToTransaktionDto (Transaktion transaktion);
 }
